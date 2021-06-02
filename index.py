5d1ad35781776dae3d3fda0ed5b1137cd530d421
luagi98 <luis.garcia_1998@hotmail.com>
2021-06-02 17:33:29 -0500

    Added insertions on database in encrypted and decrypted view

--- index.py
+++ index.py
@@ -1,8 +1,29 @@
 from vigenere import *
 from flask import Flask
 from flask import render_template,redirect,request
+from flask_sqlalchemy import SQLAlchemy
 from flask.helpers import url_for
+from datetime import date
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:@localhost:3306/vigenere'
+bd = SQLAlchemy(app)
+
+class Encrypted(bd.Model):
+     __tablename__='encrypted'
+     id = bd.Column(bd.Integer,primary_key=True)
+     originalText = bd.Column(bd.String(100))
+     encryptedText = bd.Column(bd.String(100))
+     dateEncrypted = bd.Column(bd.Date)
+
+class Decrypted(bd.Model):
+     __tablename__='decrypted'
+     id = bd.Column(bd.Integer,primary_key=True)
+     encryptedText = bd.Column(bd.String(100))
+     decryptedText = bd.Column(bd.String(100))
+     dateEncrypted = bd.Column(bd.Date)
+     
+
+
 
 # Main route, there is a profile card about me and my contact information
 
@@ -24,6 +45,10 @@ def encrypt():
           text = request.form.get("textInput")
           password = request.form.get("cipherPassword")
           encryptedText = code(text,password,["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+          bd.session.rollback()
+          enc = Encrypted(originalText=text,encryptedText=encryptedText,dateEncrypted=date.today())
+          bd.session.add(enc)
+          bd.session.commit()
           return render_template("crypt.html",method="encrypt",flag=True,cryptedText = encryptedText)
      elif request.method == "GET":
           return render_template('crypt.html',method="encrypt",flag=False)
@@ -36,6 +61,10 @@ def decrypt():
           text = request.form.get("textInput")
           password = request.form.get("cipherPassword")
           decryptedText = decode(text,password,["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+          bd.session.rollback()
+          dec = Decrypted(encryptedText=text,decryptedText=decryptedText,datedecrypted=date.today())
+          bd.session.add(dec)
+          bd.session.commit()
           return render_template("crypt.html",method="decrypt",flag=True,cryptedText = decryptedText)
      elif request.method == "GET":
           return render_template('crypt.html',method="decrypt",flag=False)
